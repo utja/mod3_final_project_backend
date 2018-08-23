2ef58e7b303a99e46f2d944a09ce065b45b77086
Jon Ahn <28519719+utja@users.noreply.github.com>
2018-08-23 11:30:19 -0400

    add serializers

--- Jetpack/app/controllers/api/v1/scores_controller.rb
+++ Jetpack/app/controllers/api/v1/scores_controller.rb
@@ -4,4 +4,15 @@ class Api::V1::ScoresController < ApplicationController
     render json: Score.all
   end
 
+  def create
+    score = Score.create(score_params)
+    render json: score, status: 201
+  end
+
+  private
+
+  def score_params
+    params.permit(:user_id, :score)
+  end
+
 end
---
+++ Jetpack/app/serializers/score_serializer.rb
@@ -0,0 +1,4 @@
+class ScoreSerializer < ActiveModel::Serializer
+  attributes :id, :score
+  belongs_to :user
+end
